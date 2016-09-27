f9b02fc7200f94aded1d24c54a21af3ed44b78cc
oftl <github@rockandroll.at>
2016-09-27 21:27:52 +0200

    redirect (303) when requesting /

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
-from bottle import route, run, template
+from bottle import route, run, template, response
 from main import Lemmy
 
 @route ('/ist/<sort_col>/<sort>')
 def index_ist (sort_col, sort):
     return Lemmy (sort_col = sort_col, sort = sort).table()
+
+@route ('/')
+def index ():
+    response.status = 303
+    response.set_header('Location', '/ist/id/asc')
 
 run (host = 'localhost', port = 8080, reloader = True)
